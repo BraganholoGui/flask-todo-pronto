1eaffcc349356e73a164b285446f861d823f8409
Guilherme Braganholo <braganhologuilherme@gmail.com>
2020-11-16 23:50:11 +0000

    Alteração dos pacotes.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from routes.Web import web
 
@@ -5,4 +6,5 @@ app = Flask(__name__)
 app.register_blueprint(web)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
