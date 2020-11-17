8aeee13c2e533db08d064477f5b496b713226fd7
Guilherme Braganholo <braganhologuilherme@gmail.com>
2020-11-17 00:00:38 +0000

    main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 app.register_blueprint(web)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+ port = int(os.environ.get("PORT", 5000))
+ app.run(host='0.0.0.0', port=port)
